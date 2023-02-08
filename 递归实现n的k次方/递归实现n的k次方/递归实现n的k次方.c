15f53c905a42970a715acb84c470c19a4c3e021d
cst_9110 <cst2209157@xmu.edu.my>
2023-02-08 21:15:39 +0800

    去

--- 递归实现n的k次方/递归实现n的k次方/递归实现n的k次方.c
+++ 递归实现n的k次方/递归实现n的k次方/递归实现n的k次方.c
@@ -6,16 +6,19 @@ int my_pow(int x, int y)
 {
 	if (y > 0)
 		return x * my_pow(x, --y);
+	else if (y < 0)
+		return 1.0 / my_pow(x, -y);
 	else
-		return 1;  
+		return 1.0;  
 }
 
 int main()
 {
 	int n, k;
 	scanf("%d%d", &n, &k);
-
-	printf("%d的%d次方等于%d",n,k,my_pow(n, k));
+	double ret = 0.0;
+	ret = my_pow(n, k);
+	printf("%d的%d次方等于%d",n,k,ret);
 	
 	return 0;
 }
--- 递归实现字符串逆序/递归实现字符串逆序/字符串逆序.cpp
+++ 递归实现字符串逆序/递归实现字符串逆序/字符串逆序.cpp
@@ -14,21 +14,21 @@
 //逆序之后数组的内容变成：fedcba
 #include <stdio.h>
 #include <string.h>
-void reverse_string(char* a)
-{
-
-	int sz = strlen(a);
-	char t = *a;
-	*a = *(a + sz - 1);
-	*(a + sz - 1) = '\0';
-
-	if (sz - 2 >= 2)
-	{
-		reverse_string(a + 1);
-	}
-
-	*(a + sz - 1) = t;
-}
+//void reverse_string(char* a)
+//{
+//
+//	int sz = strlen(a);
+//	char t = *a;
+//	*a = *(a + sz - 1);
+//	*(a + sz - 1) = '\0';
+//
+//	if (sz - 2 >= 2)
+//	{
+//		reverse_string(a + 1);
+//	}
+//
+//	*(a + sz - 1) = t;
+//}
 //int sz = strlen(a);
 //char t = *a;
 //*a = *(a + sz - 1);
@@ -38,17 +38,17 @@ void reverse_string(char* a)
 //而且数组右边“\0”每次都会增加一，每递推一次数组右边的值都会相应的赋给左边，
 //而左边的值去哪里呢？没错都“暂存起来了”，当回归时暂存的数值将会重新赋给右边，(a + sz - 1) = t; 
 //因为在回归时这行代码才会执行
-int main()
-{
-	char arr[] = "abcdef";
-	char* str = arr;
-
-	reverse_string(str);
-
-	printf("%s", arr);
-
-	return 0;
-}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* str = arr;
+//
+//	reverse_string(str);
+//
+//	printf("%s", arr);
+//
+//	return 0;
+//}
 
 
 
@@ -61,23 +61,23 @@ int main()
   2. 交换两个指针位置上的字符
   3. left指针往后走，right指针往前走，只要两个指针没有相遇，继续2，两个指针相遇后，逆置结束
 */
-void reverse_string(char* arr)
-{
-	char* left = arr;
-	char* right = arr + strlen(arr) - 1;
-
-
-	while (left < right)
-	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-
-
-		left++;
-		right--;
-	}
-}
+//void reverse_string(char* arr)
+//{
+//	char* left = arr;
+//	char* right = arr + strlen(arr) - 1;
+//
+//
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//
+//
+//		left++;
+//		right--;
+//	}
+//}
 
 
 /*
@@ -86,15 +86,49 @@ void reverse_string(char* arr)
   1. 交换a和g，
   2. 以递归的方式逆置源字符串的剩余部分，剩余部分可以看成一个有效的字符串，再以类似的方式逆置
 */
-void reverse_string(char* arr)
+//void reverse_string(char* arr)
+//{
+//	int len = strlen(arr);
+//	char tmp = *arr;
+//	*arr = *(arr + len - 1);
+//
+//	*(arr + len - 1) = '\0';
+//	if (strlen(arr + 1) >= 2)
+//		reverse_string(arr + 1);
+//
+//	*(arr + len - 1) = tmp;
+//}
+
+//void reverse_string(char* arr)
+//{
+//	char* left = arr;
+//	char* right = arr + strlen(arr) - 1;
+//
+//
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* str = arr;
+//
+//	reverse_string(str);
+//
+//	printf("%s", arr);
+//
+//	return 0;
+//}
+#include <stdio.h>
+int main()
 {
-	int len = strlen(arr);
-	char tmp = *arr;
-	*arr = *(arr + len - 1);
-
-	*(arr + len - 1) = '\0';
-	if (strlen(arr + 1) >= 2)
-		reverse_string(arr + 1);
-
-	*(arr + len - 1) = tmp;
+    char str[] = "hello bit";
+    printf("%d %d\n", sizeof(str), strlen(str));
+    return 0;
 }
